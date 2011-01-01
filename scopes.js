b1b0463464e26b52b2fb1acaf30ca18ec856a5c0
Aditya <adityapal8763244@gmail.com>
2011-01-01 08:22:54 +0530

    A new file added scope.js

---
+++ scopes.js
@@ -0,0 +1,19 @@
+// Scopes in JavaScript
+// The scope is by default global
+// var name="Amal";
+
+// function sayName(){
+//     var marks=10;
+//     console.log(name);
+// }
+// sayName();
+// consol.log(marks);
+
+
+
+
+
+
+
+
+// Global 
