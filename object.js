b8be1f480e92fee43922d81e57edafd374f73ff0
Aditya <adityapal8763244@gmail.com>
2022-03-16 13:15:07 +0530

    updated object.js

--- object.js
+++ object.js
@@ -36,19 +36,74 @@ function createCourse(title) {
 
 // contructor function
 function Course(title) {
-  (this.title = title),
+    (this.title = title),
     (this.enroll = function () {
       console.log("You are sucessfully enrolled");
     });
 }
-const course = new Course("JavaScript");
+// const course = new Course("JavaScript");
 // To delete a function
-delete course.title;
-// To create a function
-course.checkEnrollment = function () {
-  console.log("30 users are enrolled");
-};
-console.log(course);
+// delete course.title;
 
-course.checkEnrollment();
-course.enroll();
+// To create a function
+// course.checkEnrollment = function () {
+  // console.log("30 users are enrolled");
+// };
+// console.log(course);
+
+// course.checkEnrollment();
+// course.enroll();
+
+
+
+const Course_1= new Function('title',`
+this.title = title,
+this.enroll = function () {
+  console.log("You are sucessfully enrolled");
+} 
+`)
+const course_2 = new Course_1('JavaScript');
+course_2.enroll();
+
+
+// Primitive Datatype
+// let number =10;
+// Pass by value
+// let number_2=number
+// number=15
+
+// console.log(number)
+// console.log(number_2)
+
+//Reference Datatype
+// let obj={ number:10}
+
+//Pass by Reference
+// let obj_2=obj;
+// obj.number=15;
+
+// console.log(obj);
+// console.log(obj_2);
+
+const course={
+  title:'JavaScript',
+  enroll() {
+    console.log("You are sucessfully enrolled");
+  }
+}
+// const course_1={...course}
+// course_1.title="C++";
+
+// const course_1=Object.assign({},course)
+// course_1.title="C++";
+
+// for in loop
+// for (let key in course){
+//   console.log(key , course[key]);
+// } 
+
+const course_1= {};
+// for of loop
+for(let key of Object.keys(course)){
+  course_1[key]= course[key]
+}
