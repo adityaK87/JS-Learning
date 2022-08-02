c3980d4c690b25c3542a1d6169f406f638f100f7
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-08-02 09:59:29 +0530

    new examples added

--- Callbacks and Promises/example.js
+++ Callbacks and Promises/example.js
@@ -1,76 +1,78 @@
-
-// example --1
-
-// console.log(1) // 1️⃣ 
-
-// setTimeout(() => {
-//   console.log(2) // 1️⃣1️⃣
-// }, 10)
-
-// setTimeout(() => {
-//   console.log(3) // 1️⃣0️⃣
-// }, 0);
-
-// new Promise((_, reject) => {
-    // console.log(4) // 2️⃣
-    // reject(5)
-    // console.log(6) // 3️⃣
-// })
-// .then(() => console.log(7))
-// .catch(() => console.log(8)) // 5️⃣
-// .then(() => console.log(9)) // 6️⃣
-// .catch(() => console.log(10))
-// .then(() => console.log(11)) // 7️⃣
-// .then(console.log) // 8️⃣
-// .finally(() => console.log(12)) // 9️⃣
-
-// console.log(13) // 4️⃣
-
-
-
-
-// Example-2
-
-// const p1 = Promise.resolve(1)
-// const p2 = new Promise((resolve) => resolve(p1))
-// const p3 = Promise.resolve(p1)
-// const p4 = p2.then(() => new Promise((resolve) => resolve(p3)))
-// const p5 = p4.then(() => p4)
-
-// console.log(p1 == p2) // false
-// console.log(p1 == p3) // true
-// console.log(p3 == p4) // false
-// console.log(p4 == p5) // false
+// Example--1
+async function async1(){
+    console.log(1)
+    await async2()
+    console.log(2)
+  }
+  
+  async function async2(){
+    console.log(3)
+  }
+  
+  console.log(4)
+  
+  setTimeout(function(){
+    console.log(5)
+  }, 0)
+  
+  async1()
+  
+  new Promise(function(resolve){
+    console.log(6)
+    resolve()
+  }).then(function(){
+    console.log(7)
+  })
+  
+  console.log(8)
 
 
 
-// // Example--3
 
-// console.log(1)
-// const promise = new Promise((resolve) => {
-//   console.log(2)
-//   resolve()
-//   console.log(3)
-// })
+Example-2
 
-// console.log(4)
+const p1 = Promise.resolve(1)
+const p2 = new Promise((resolve) => resolve(p1))
+const p3 = Promise.resolve(p1)
+const p4 = p2.then(() => new Promise((resolve) => resolve(p3)))
+const p5 = p4.then(() => p4)
 
-// promise.then(() => {
-//   console.log(5)
-// }).then(() => {
-//   console.log(6)
-// })
+console.log(p1 == p2) // false
+console.log(p1 == p3) // true
+console.log(p3 == p4) // false
+console.log(p4 == p5) // false
 
-// console.log(7)
 
-// setTimeout(() => {
-//   console.log(8)
-// }, 10)
 
-// setTimeout(() => {
-//   console.log(9)
-// }, 0)
+// Example--3
 
+console.log(1)
+const promise = new Promise((resolve) => {
+  console.log(2)
+  resolve()
+  console.log(3)
+})
+
+console.log(4)
+
+promise.then(() => {
+  console.log(5)
+}).then(() => {
+  console.log(6)
+})
+
+console.log(7)
+
+setTimeout(() => {
+  console.log(8)
+}, 10)
+
+setTimeout(() => {
+  console.log(9)
+}, 0)
+
+
+// Example--4
 
 Promise.resolve(1)
 .then(() => 2)
@@ -78,3 +80,50 @@ Promise.resolve(1)
 .then((value) => value * 3)
 .then(Promise.resolve(4))
 .then(console.log)
+
+
+Example--5
+
+Promise.resolve(1)
+.then((val) => {
+  console.log(val)
+  return val + 1
+}).then((val) => {
+  console.log(val)
+}).then((val) => {
+  console.log(val)
+  return Promise.resolve(3)
+    .then((val) => {
+      console.log(val)
+    })
+}).then((val) => {
+  console.log(val)
+  return Promise.reject(4)
+}).catch((val) => {
+  console.log(val)
+}).finally((val) => {
+  console.log(val)
+  return 10
+}).then((val) => {
+  console.log(val)
+})
+
+
+Example-6
+
+Promise.resolve(1)
+.finally((data) => {
+  console.log(data)
+  return Promise.reject('error')
+})
+.catch((error) => {
+  console.log(error)
+  throw 'error2'
+})
+.finally((data) => {
+  console.log(data)
+  return Promise.resolve(2).then(console.log)
+})
+.then(console.log)
+.catch(console.log)
+
