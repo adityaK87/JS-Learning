774a472e2c25d1582c909ff3d0dd652de4fa1024
Aditya kumar <adityapal8763244@gmail.com>
2022-10-28 19:38:07 +0530

    study about tryCatch

---
+++ tryCatch.js
@@ -0,0 +1,21 @@
+setTimeout(() => {
+    console.log("Hacking wifi... Please wait...")
+}, 1000);
+
+try {
+    console.log(rahul)
+} catch (error) {
+    console.log(error)
+}
+
+setTimeout(() => {
+    console.log("Fetching userName and Password... Please wait...")
+}, 2000);
+
+setTimeout(() => {
+    console.log("Hacking Rahul's faceBook id... Please wait...")
+}, 3000);
+
+setTimeout(() => {
+    console.log("Hacked Rahul's faceBook id ")
+}, 4000);
