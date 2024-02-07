b042190ca5ff04be68f5f26edc26715a9a4d2a65
Aditya Kumar <adityapal1409@gmail.com>
2024-02-07 21:26:17 +0530

    important closure question

--- closure.js
+++ closure.js
@@ -35,12 +35,49 @@
 // let a = returnFunc();
 // a();
 
-function x() {
-	var a = 7;
-	function y() {
-		console.log(a);
-	}
-	return y;
-}
+// function x() {
+// 	var a = 7;
+// 	function y() {
+// 		console.log(a);
+// 	}
+// 	return y;
+// }
 
-x()();
+// x()();
+
+// function x() {
+// 	for (var i = 1; i <= 5; i++) {
+// 		setTimeout(function () {
+// 			console.log(i);
+// 		}, i * 1000);
+// 	}
+// 	console.log("Namaste Javascript");
+// }
+// x();
+// Output:
+// Namaste Javascript
+// 6
+// 6
+// 6
+// 6
+// 6
+
+// Reason? => This happens because of closures. When setTimeout stores the function somewhere and attaches timer to it, the function remembers its reference to i, not value of i. All 5 copies of function point to same reference of i. JS stores these 5 functions, prints string and then comes back to the functions. By then the timer has run fully. And due to looping, the i value became 6. And when the callback fun runs the variable i = 6. So same 6 is printed in each log
+
+// To avoid this, we can use let instead of var as let has Block scope. For each iteration, the i is a new variable altogether(new copy of i). Everytime setTimeout is run, the inside function forms closure with new variable i
+
+// But what if interviewer ask us to implement using var?
+
+function x() {
+	for (var i = 1; i <= 5; i++) {
+		function close(i) {
+			setTimeout(function () {
+				console.log(i);
+			}, i * 1000);
+			// put the setT function inside new function close()
+		}
+		close(i); // everytime you call close(i) it creates new copy of i. Only this time, it is with var itself!
+	}
+	console.log("Namaste Javascript");
+}
+x();
